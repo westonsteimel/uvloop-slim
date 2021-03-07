eeed8b519891d3d277423f9ff679107c301c4e49
Weston Steimel <weston.steimel@gmail.com>
2021-03-07 00:46:14 +0000

    drop python 3.5, 3.6 and add 3.9
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- slimify.sh
+++ slimify.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+pip install -U pip==20.2.4
+
 PIP_DOWNLOAD_CMD="pip download --no-deps --disable-pip-version-check"
 
 mkdir -p dist
@@ -10,15 +12,15 @@ mkdir -p dist
     cd dist
 
     if [[ -z "${UVLOOP_VERSION}" ]]; then
-        UVLOOP_VERSION=$(pip search uvloop | pcregrep -o1 -e "^uvloop \((.*)\).*$")
+        echo "Set the UVLOOP_VERSION environment variable."
+        exit 1
     fi
 
     echo "slimming wheels for uvloop version ${UVLOOP_VERSION}"
     
+    $PIP_DOWNLOAD_CMD --python-version 3.9 --platform manylinux2010_x86_64 uvloop==${UVLOOP_VERSION}
     $PIP_DOWNLOAD_CMD --python-version 3.8 --platform manylinux2010_x86_64 uvloop==${UVLOOP_VERSION}
     $PIP_DOWNLOAD_CMD --python-version 3.7 --platform manylinux2010_x86_64 uvloop==${UVLOOP_VERSION}
-    $PIP_DOWNLOAD_CMD --python-version 3.6 --platform manylinux2010_x86_64 uvloop==${UVLOOP_VERSION}
-    $PIP_DOWNLOAD_CMD --python-version 3.5 --platform manylinux2010_x86_64 uvloop==${UVLOOP_VERSION}
 
     for filename in ./*.whl
     do
@@ -31,7 +33,11 @@ mkdir -p dist
     done
 
     pip uninstall -y --disable-pip-version-check uvloop
-    pip install --disable-pip-version-check uvloop -f .
+    pip install \
+        --disable-pip-version-check uvloop==${UVLOOP_VERSION} \
+        -f . \
+        --index-url https://westonsteimel.github.io/pypi-repo \
+        --extra-index-url https://pypi.org/pypi
 
     python -c "
 import uvloop
